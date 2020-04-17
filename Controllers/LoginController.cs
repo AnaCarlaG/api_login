14b32e2411b889c2495bc23a8db176324c39f781
AnaCarlaG <47726766+AnaCarlaG@users.noreply.github.com>
2020-04-16 22:35:24 -0300

    Inicial Commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Login.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Controllers
+{
+    [ApiController]
+    public class LoginController :Controller
+    {
+        LoginModel model = new LoginModel();
+        [HttpGet]
+        public ActionResult<LoginModel> logar(string senha, string user)
+        {
+            foreach(LoginModel model in model.list_login){
+                if(model.senha == senha && model.user == user)
+                {
+                    var objLogin = model;
+                    return Ok(objLogin);
+                }
+                return NotFound();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ Model/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Model
+{
+    public class LoginModel
+    {
+        public string user { get; set; }
+        public string senha { get; }
+        public IEnumerable<LoginModel> list_login { get; set; }
+    }
+}
